be373bceb76a90a48de41b2ed29992b9bd0ae1fe
vasyagina <margo.vasyagina@yandex.ru>
2022-06-07 17:03:02 +0300

    test tasks

---
+++ task_1.py
@@ -0,0 +1,11 @@
+def my_code(wlib):
+    for key, value in wlib.items():
+        if type(wlib[key]) == str:
+            print(key + ": ", value, sep =' \n \t')
+        else:
+            my_code(value)
+
+lib1 = {'first': 'first_value', 'second': 'second_value'}
+lib2= { '1': { 'child': '1/child/value' }, '2': '2/value'}
+
+my_code(lib1)
---
+++ task_2.py
@@ -0,0 +1,24 @@
+def return_arr(graph, start, path = []):
+    path.append(start)
+    if start in graph:
+        for elem in graph:
+            if graph[elem] not in path:
+                for el in graph[elem]:
+                    if el not in path:
+                        path.append(el)
+    return path
+
+def my_code(graph, start):
+    path = return_arr(graph, start)
+    print(*path, sep='\n')
+
+data1 = {
+ 1: [2, 3],
+ 2: [4]
+}
+data2 = {
+ 1: [2, 3],
+ 2: [3, 4],
+ 4: [1]
+}
+my_code(data1, 1)
